5e339875d2631e52d213c84daf3bc3e2be9df1e3
Lee Min Woo <lmw7414@naver.com>
2023-04-27 13:54:28 +0900

    초기 세팅

---
+++ src/main/java/dev/be/async/PracticeAsyncApplication.java
@@ -0,0 +1,13 @@
+package dev.be.async;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeAsyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeAsyncApplication.class, args);
+    }
+
+}
