d306acb62f786f8926b87e9cb983b01d8f536f37
Lee Min Woo <lmw7414@naver.com>
2023-05-02 15:47:16 +0900

    임포트문 제거

--- src/main/java/dev/be/async/Service/AsyncService.java
+++ src/main/java/dev/be/async/Service/AsyncService.java
@@ -2,13 +2,9 @@ package dev.be.async.Service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
 import java.util.stream.IntStream;
 
 @Slf4j
--- src/main/java/dev/be/async/domain/Coffee.java
+++ src/main/java/dev/be/async/domain/Coffee.java
@@ -2,7 +2,6 @@ package dev.be.async.domain;
 
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 
 @Data
 @Builder
